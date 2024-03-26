b1dc5d79ab4a7cab1dd5eba4178ed7731f5609f7
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2024-03-26 16:16:00 -0300

    Configura o acesso ao BD PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatosController } from './gatos/gatos.controller';
 import { GatosService } from './gatos/gatos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-co1gfp20si5c739ppavg-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 'banco_teste_q7hk_user',
+      password: '123',
+      database: 'banco_teste_q7hk',
+      entities: [],
+      synchronize: true,
+      ssl: true
+    }),    
+  ],
   controllers: [AppController, GatosController],
   providers: [AppService, GatosService],
 })
